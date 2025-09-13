6a3f512c7792631598d1693903f8831f8c62934d
JMDM <jmo.880@gmail.com>
2025-09-13 18:55:12 +0200

    Add: configuration files for docs

---
+++ docs/conf.py
@@ -0,0 +1,13 @@
+"""Sphinx configuration."""
+
+html_theme = "shibuya"
+
+project = "Example Project (to Learn)"
+
+author = "jmdm"
+
+copyright = "2025, jmdm"  # noqa: A001
+
+extensions = [
+    "myst_parser",
+]
